29e701a6e81e34d169c493724ea3cd8f5e47c82c
MTurnhout <MTurnhout@users.noreply.github.com>
2023-03-31 20:31:06 +0200

    Added icons

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 const gulp = require("gulp");
 const zip = require("gulp-zip");
+const pjson = require("./package.json");
 
 exports.default = () =>
   gulp
     .src(["src/**", "!src/**/*.ts"])
-    .pipe(zip("archive.zip"))
+    .pipe(zip(`v${pjson.version}.zip`))
     .pipe(gulp.dest("dist"));
