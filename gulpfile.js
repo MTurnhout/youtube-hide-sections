7a28810a1aebacb051f4f1c8949adaaf8a6b6538
MTurnhout <MTurnhout@users.noreply.github.com>
2023-04-07 15:43:08 +0200

    Updated gulpfile.js to fix image issue

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
-const gulp = require("gulp");
+const { src, dest } = require("gulp");
 const gulp_remove_logging = require("gulp-remove-logging");
 const zip = require("gulp-zip");
 const pjson = require("./package.json");
 
 exports.default = () =>
-  gulp
-    .src(["src/**", "!src/**/*.ts"])
+  src("src/**/*.js")
     .pipe(gulp_remove_logging())
+    .pipe(src(["src/**", "!src/**/*.ts", "!src/**/*.js"]))
     .pipe(zip(`v${pjson.version}.zip`))
-    .pipe(gulp.dest("dist"));
+    .pipe(dest("dist"));
